2bc3e62fdc35a099bbb7014af97a7745b4f8120e
Wian <117603788+Unknown-S48@users.noreply.github.com>
2026-04-23 10:31:24 +0200

    Adding needed file for Question 2
    I added the starter code and the comments with sudo code.

---
+++ Question_2.cs
@@ -0,0 +1,15 @@
+//Wian Sandenbergh
+//Question 2
+/*
+Develop a Simple ATM Console Application using C# that simulates a basic banking withdrawal transaction. 
+The system must interact with the user, accept inputs, perform calculations, and display transaction results including the updated balance and transaction time.
+*/
+using System;
+using System.Globalization;
+class Question_2
+{
+	static void Main()
+	{
+
+	}
+}
