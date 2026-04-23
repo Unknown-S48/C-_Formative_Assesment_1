69f94056bed21d07ee27ee0d6c1133076d62b65c
Wian <117603788+Unknown-S48@users.noreply.github.com>
2026-04-23 05:07:44 +0200

    Basic Start
    Added the sudo code for the file in comments giving me a rough idea of what to do in the project and I added the main classes code that will be used.

---
+++ Question_1.cs
@@ -0,0 +1,22 @@
+//Wian Sandenbergh
+//Question 1
+
+/*
+Prompt the user to enter a student name
+Prompt the user to enter three subject marks
+Validate that marks entered are numeric values 
+Calculate the total marks
+Calculate the average marks
+Display whether the student PASS or FAIL
+Rule: Average >= 50 = PASS
+Dispaly results in the format shown above
+*/
+using System;
+using System.Globalization;
+class Question_1
+{
+	static void Main()
+	{
+
+	}
+}
